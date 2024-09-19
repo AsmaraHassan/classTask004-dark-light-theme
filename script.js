15e52c374ae8e1178b6e958ea4a737bf1b343238
AsmaraHassan <asmarahassan939@gmail.com>
2024-09-19 02:34:18 -0700

    5th commit

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ var mode = document.getElementById(center-button)
 mode.innerHTML ='Night-mode';
  var changecolor=document.getElementById(center-button).addEventListener('click' , function(){
 
+if(document.body.style.background === 'black'){
+    document.body.style.background ='white';
+    mode.style.backgroundColor ='black';
+    mode.style.color ='white';
+    mode.innerHTML = 'Night';
+}
+else {
+    document.body.style.backgroundColor = 'black';
+    mode.style.backgroundColor = 'white';
+    mode.style.color = 'black';
+    mode.innerHTML = 'Day';
+}
+
 })
