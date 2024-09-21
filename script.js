1276584c217e2af3683d0596f67fba4bdbe54d05
AsmaraHassan <asmarahassan939@gmail.com>
2024-09-21 15:35:23 -0700

    7th commit

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-// var mode = document.getElementById(center-button)
-// mode.innerHTML ='Night-mode';
-//  var changecolor=document.getElementById(center-button).addEventListener('onclick' , function(){
 
-// if(document.body.style.background === 'black'){
-//     document.body.style.background ='white';
-//     mode.style.backgroundColor ='black';
-//     mode.style.color ='white';
-//     mode.innerHTML = 'Night';
-// }
-// else {
-//     document.body.style.backgroundColor = 'black';
-//     mode.style.backgroundColor = 'white';
-//     mode.style.color = 'black';
-//     mode.innerHTML = 'Day';
-// }
-
-// })
 var button = document.getElementById('btn');
 button.innerHTML = 'Night';
 var colorchnager =  document.getElementById('btn').addEventListener('click', function(){
@@ -23,11 +6,11 @@ var colorchnager =  document.getElementById('btn').addEventListener('click', fun
         document.body.style.backgroundColor = 'white';
         button.style.backgroundColor = 'black';
         button.style.color = 'white';
-        button.innerHTML = 'Night';
+        button.innerHTML = 'Day';
     } else {
         document.body.style.backgroundColor = 'black';
         button.style.backgroundColor = 'white';
         button.style.color = 'black';
-        button.innerHTML = 'Day';
+        button.innerHTML = 'Night';
     }
 })
